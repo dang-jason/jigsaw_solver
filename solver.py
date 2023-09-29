5c7de3523a4f55bddb14dcc065ea4733744d325d
Jason Dang <jasondang619@gmail.com>
2023-09-29 17:21:53 -0500

    ladybug puzzle

--- solver.py
+++ solver.py
@@ -1,3 +1,13 @@
 import numpy as np
 import cv2
+from PIL import Image
+from rembg import remove
 
+
+
+img = cv2.imread('puzzles/ladybug.png')
+channels = img.split()
+print(channels.shape)
+
+
+cv2.waitKey(0)
