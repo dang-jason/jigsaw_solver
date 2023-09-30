2e06d2cb6d7d6439ee106ee45ea6f3100d83ba42
RaviPatel021 <ravi_patel@utexas.edu>
2023-09-29 19:09:44 -0500

    background added

--- solver.py
+++ solver.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
-from PIL import Image
-from rembg import remove
+# from PIL import Image
+# from rembg import remove
 
 
 
 img = cv2.imread('puzzles/ladybug.png')
-channels = img.split()
-print(channels.shape)
+
+cv2.imshow('image',img)
+
+
 
 
 cv2.waitKey(0)
